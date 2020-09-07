3ab491667402b0772d6552b92b291ba1e82ef4db
Lorenalgm <lorena@proesc.com>
2020-09-07 13:50:58 -0300

    Exibida api oficial

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://megahackwomen.herokuapp.com'
-    baseURL: 'http://192.168.1.106:3333'
+    baseURL: 'https://megahackwomen.herokuapp.com'
+    // baseURL: 'http://192.168.1.106:3333'
 })
 
 export default api;
